2e445672efacac3aada408c6dcf0411fd22235b7
Niall Noonan <noonanni@tcd.ie>
2021-01-23 00:43:16 -0500

    Update Loadcsv.py

--- Loadcsv.py
+++ Loadcsv.py
@@ -3,23 +3,41 @@ import tabulate as tb
 import matplotlib.pyplot as plt
 
 
+countries = ['Ireland','Scotland', 'France', 'Brazil']
+
 df = pd.read_csv(
 'CoronaData.csv', 
 low_memory=False,
 dtype={"value": float},
-usecols=['country', 'type'],
+usecols=['country','type','value'],
 na_values =['.','??']
 )
 
 #Corona.groupby('country').value.sum().plot(kind='bar')
 #df.groupby('country','type').plot(kind='bar')
-df.groupby('country').sum('type').plot(kind='bar')
-
+#df.groupby('country').type.sum().plot(kind='bar')
+#idx = df.groupby(['country'])['value'].transform(max) == df['value']
+#max_value = value. max() get max value from "col2"
 #plt.plot(Corona['country'], Corona['population'], Corona['date'])
+grouped = df.groupby(['country','type']).sum()
 
 
-plt.show()
 
+#df.plot(x ='country', kind = 'line')
+
+#newdf('country').plot(kind="bar")
+
+
+print(grouped)
+grouped.to_csv("groupedtest.csv")  
+
+
+
+
+#pnewdf.plot.hist();
+#pplt.show()
+
+#print(newdf)
 #x = Corona.country
 #y = Corona.population
 #plt.plot(x,y)
