2f10c361d0444f2a568895eee520e9df6a1112a8
Niall Noonan <noonanni@tcd.ie>
2021-01-24 22:09:48 -0500

    Update Loadcsv.py

--- Loadcsv.py
+++ Loadcsv.py
@@ -1,49 +1,57 @@
 import pandas as pd
 import tabulate as tb
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+import pandas as pd
 
 
-countries = ['Ireland','Scotland', 'France', 'Brazil']
-
-df = pd.read_csv(
-'CoronaData.csv', 
+def import_csv_data():
+    global v
+    csv_file_path = askopenfilename()
+    print(csv_file_path)
+    v.set(csv_file_path)
+    df = pd.read_csv(csv_file_path, 
 low_memory=False,
 dtype={"value": float},
 usecols=['country','type','value'],
 na_values =['.','??']
 )
 
+
+root = tk.Tk()
+tk.Label(root, text='File Path').grid(row=0, column=0)
+v = tk.StringVar()
+entry = tk.Entry(root, textvariable=v).grid(row=0, column=1)
+tk.Button(root, text='Browse Data Set',command=import_csv_data).grid(row=1, column=0)
+tk.Button(root, text='Close',command=root.destroy).grid(row=1, column=1)
+root.mainloop()
+
+df_columns = df.columns.tolist()
+print(df_columns)
+
+
+
+
+#countries = ['Ireland','Scotland', 'France', 'Brazil']
 #Corona.groupby('country').value.sum().plot(kind='bar')
 #df.groupby('country','type').plot(kind='bar')
 #df.groupby('country').type.sum().plot(kind='bar')
 #idx = df.groupby(['country'])['value'].transform(max) == df['value']
 #max_value = value. max() get max value from "col2"
 #plt.plot(Corona['country'], Corona['population'], Corona['date'])
-grouped = df.groupby(['country','type']).sum()
-
-
-
+#grouped = df.groupby(['country','type']).sum()
 #df.plot(x ='country', kind = 'line')
-
 #newdf('country').plot(kind="bar")
-
-
-print(grouped)
-grouped.to_csv("groupedtest.csv")  
-
-
-
 
 #pnewdf.plot.hist();
 #pplt.show()
-
 #print(newdf)
 #x = Corona.country
 #y = Corona.population
 #plt.plot(x,y)
 #plt.scatter(x, y)
 #plt.show(kind=bar)
-
 #plot(col1, col2)
 #print(Corona)
 
