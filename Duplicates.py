d0e973277b454349a87062bfcaac68768433bf5d
Niall Noonan <noonanni@tcd.ie>
2021-01-25 23:55:13 -0500

    Update Duplicates.py

--- Duplicates.py
+++ Duplicates.py
@@ -14,10 +14,28 @@ df = pd.read_csv('CorData.csv',low_memory=False)
 
 df_columns = df.columns.tolist()
 
-df_duplicates_specific = df.duplicated(subset=['year_week', 'cases_weekly'])
+#Specific Columns
+df_duplicates = df.duplicated(keep=False)
+dupe_row = df_duplicates[df_duplicates.duplicated()]
 
 
-print(df_duplicates)
+#All Columns
+df_duplicates_col = df.duplicated(keep=False,subset=['year_week', 'cases_weekly'])
+dupe_row_col = df_duplicates_col[df_duplicates_col.duplicated()]
 
+#Average_val = df.groupby('country').mean()
+
+
+pd.set_option('display.max_colwidth', None) 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 2000)
+pd.set_option('display.float_format', '{:20,.2f}'.format)
+
+
+
+print(df)
+print(df_duplicates_col)
+#print(Average_val)
 
 
